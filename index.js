ad426fdafa93d4632dd501e35db9f9d23c32360f
Ketcap <uoruc5@hotmail.com>
2018-08-12 19:19:59 +0200

    updated

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ import cors from 'cors';
 import Routes from './routes';
 
 Routes.map(r => {
-	fastify.route({ ...r });
+  fastify.route({ ...r });
 })
 // Run the server!
+fastify.use(cors());
+
 const start = async () => {
-	try {
-		await fastify.listen(8000)
-		fastify.log.info(`server listening on ${fastify.server.address().port}`)
-	} catch (err) {
-		fastify.log.error(err)
-		process.exit(1)
-	}
+  try {
+    await fastify.listen(8000)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
 }
 start()
--- routes/events.js
+++ routes/events.js
@@ -1,49 +1,50 @@
 import faker from 'faker';
 
 Date.prototype.addHours = function (h) {
-	this.setTime(this.getTime() + (h * 60 * 60 * 1000));
-	return new Date(this);
+  this.setTime(this.getTime() + (h * 60 * 60 * 1000));
+  return new Date(this);
 }
 
 const generateEvent = () => {
-	const starts = faker.date.future();
-	const finishes = new Date(starts).addHours(faker.random.arrayElement([1, 1.5, 2, 2.5, 3, 3.5]))
-	return {
-		_id: faker.random.uuid(),
-		organizer: faker.random.uuid(),
-		contact: {
-			phone: faker.phone.phoneNumber(),
-			name: faker.name.firstName() + ' ' + faker.name.lastName(),
-			username: faker.internet.userName(),
-			image: 'http://placekitten.com/200/300',
-			email: faker.internet.email()
-		},
-		dates: {
-			starts, finishes,
-			// Monday Tuesday Wednesday Thursday Friday Saturday Sunday
-			repeats: faker.random.arrayElement([0, 1, 2, 3, 4, 5, 6])
-		},
-		place: {
-			name: faker.company.companyName(),
-			address: faker.address.streetAddress(),
-			city: faker.address.city()
-		},
-		price: faker.commerce.price(0, 100),
-		place: faker.random.arrayElement(['club', 'indoor', 'outdoor'])
-	}
+  const starts = faker.date.future();
+  const finishes = new Date(starts).addHours(faker.random.arrayElement([1, 1.5, 2, 2.5, 3, 3.5]))
+  return {
+    _id: faker.random.uuid(),
+    organizer: faker.random.uuid(),
+    name: faker.lorem.words(3),
+    contact: {
+      phone: faker.phone.phoneNumber(),
+      name: faker.name.firstName() + ' ' + faker.name.lastName(),
+      username: faker.internet.userName(),
+      image: 'http://placekitten.com/200/300',
+      email: faker.internet.email()
+    },
+    dates: {
+      starts, finishes,
+      // Monday Tuesday Wednesday Thursday Friday Saturday Sunday
+      repeats: faker.random.arrayElement([0, 1, 2, 3, 4, 5, 6])
+    },
+    place: {
+      name: faker.company.companyName(),
+      address: faker.address.streetAddress(),
+      city: faker.address.city()
+    },
+    price: faker.commerce.price(0, 100),
+    place: faker.random.arrayElement(['club', 'indoor', 'outdoor'])
+  }
 }
 
 
 
 export default [
-	{
-		method: 'GET',
-		url: '/events',
-		handler: async (req, rep) => {
-			rep.send(
-				new Array(20).fill('').map(generateEvent)
-			);
-		}
-	}
+  {
+    method: 'GET',
+    url: '/events',
+    handler: async (req, rep) => {
+      rep.send(
+        new Array(20).fill('').map(generateEvent)
+      );
+    }
+  }
 
 ]
